e071e4738c38f7cff3757b06405e5b74c229bbba
David Mendaño <david.mendano@movetia.com>
2018-12-10 23:37:15 +0100

    Initial

---
+++ Assets/Scripts/MenuScreen.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScreen : MonoBehaviour {
+
+    private const string SCREEN_TO_OPEN = "MenuScene";
+
+    public void LaunchMenuScreen()
+    {
+        SceneManager.LoadScene(SCREEN_TO_OPEN);
+    }
+}
---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+
+    private const string SCREEN_TO_OPEN = "PlayScene";
+
+    public void LaunchPlayScreen()
+    {
+        SceneManager.LoadScene(SCREEN_TO_OPEN);
+    }
+}
